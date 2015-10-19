8e13e65ad53db7f378f5d4d72fd481e8fd74833d
Alexander Rivera <arivepr@outlook.com>
2015-10-19 13:51:07 -0400

    Initial Commit

---
+++ LinkedListTest/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LinkedListTest
+//
+//  Created by Alexander Rivera on 10/19/15.
+//  Copyright (c) 2015 Alexander Rivera. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
